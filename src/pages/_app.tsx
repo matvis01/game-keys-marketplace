ab7d623a2e19b4eaa2d187a8d4236a0ad3a5bd08
filus4 <filej1025@gmail.com>
2023-10-29 15:49:09 +0100

    small changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/react"
 import { WagmiConfig } from "wagmi"
 import { sepolia } from "wagmi/chains"
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
+import NavBar from "@/components/Navigation/navBar"
 
 const client = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_GRAPH_URL || "",
@@ -26,6 +27,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig config={wagmiConfig}>
       <ApolloProvider client={client}>
+      <NavBar/>
         <Component {...pageProps} />
       </ApolloProvider>
     </WagmiConfig>
