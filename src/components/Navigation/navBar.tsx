eb8449d47e1796b2264eca14b87c3ee4892b73c6
filus4 <filej1025@gmail.com>
2023-10-29 17:24:15 +0100

    exported functionality of navbar options to different component

--- src/components/Navigation/navBar.tsx
+++ src/components/Navigation/navBar.tsx
@@ -1,20 +1,21 @@
 import React from "react"
 import { Image } from "next/dist/client/image-component"
 import { useRouter } from "next/router"
-import Link from "next/dist/client/link"
-import ConnectBtn from "../ConnectBtn"
+import { useAccount } from "wagmi"
 
-import navBarListItem from "./navBarListItem"
+import ConnectBtn from "../ConnectBtn"
+import NavBarListItem from "./navBarListItem"
 
 const NavBar = () => {
   const router = useRouter()
+  const { status } = useAccount()
   const { pathname } = router
   const isHome = pathname === "/"
-  const isCategories = pathname === "/categories"
-  const isCart = pathname === "/cart"
+  const isCategories = pathname.includes("/categories")
+  const isCart = pathname.includes("/cart")
 
   return (
-    <div className="sticky z-10 h-fit w-full bg-base-100 border border-white px-4 py-2 top-0 bg-opacity-80 bg-clip-padding blur-backdrop-filter">
+    <div className="sticky z-10 h-fit w-full bg-base-100 px-4 py-2 top-0 bg-opacity-80 bg-clip-padding blur-backdrop-filter">
       <nav className="flex items-center justify-between w-full max-w-screen-xl px-5 mx-auto text-white">
         <div className="flex items-center gap-8">
           <div className="w-fit">
@@ -29,33 +30,27 @@ const NavBar = () => {
             <input
               type="text"
               placeholder="Search..."
-              className="input input-sm input-bordered rounded-lg w-full max-w-xs bg-neutral"
+              className="input input-sm input-bordered w-full max-w-xs bg-neutral"
             />
           </div>
         </div>
         <div>
           <ul className="flex items-center justify-between space-x-8">
-            <li>
-              <Link href="/">
-                <p
-                  className={`text-xl font-semibold p-2 border border-transparent hover:border hover:border-primary hover:rounded-lg ${
-                    isHome && "text-primary"
-                  }`}
-                >
-                  Home
-                </p>
-              </Link>
-            </li>
-            <li>
-              <Link href="/about">
-                <p className="text-xl font-semibold">Categories</p>
-              </Link>
-            </li>
-            <li>
-              <Link href="/contact">
-                <p className="text-xl font-semibold">Cart</p>
-              </Link>
-            </li>
+            <NavBarListItem
+              text="Home"
+              href="/"
+              styles={`${isHome && "text-primary"}`}
+            />
+            <NavBarListItem
+              text="Categories"
+              href="/categories"
+              styles={`${isCategories && "text-primary"}`}
+            />
+            <NavBarListItem
+              text="Cart"
+              href="/cart"
+              styles={`${isCart && "text-primary"}`}
+            />
             <li>
               <ConnectBtn />
             </li>
