a28c84ba4fc5452736d2da41378d67c35b2b8a15
filus4 <filej1025@gmail.com>
2023-10-31 20:31:09 +0100

    small swiper card mapping and css changes

--- src/components/HomePage/FeaturedGames/FeaturedGames.tsx
+++ src/components/HomePage/FeaturedGames/FeaturedGames.tsx
@@ -7,7 +7,7 @@ import "swiper/css/free-mode"
 
 import { FreeMode, Pagination } from "swiper/modules"
 import { RxArrowTopRight } from "react-icons/rx"
-import GameCard from "./GameCard"
+import SwiperCard from "./SwiperCard"
 
 import exampleGame from "@/constants/exampleGame.json"
 import { GameType } from "@/types/gameType"
@@ -46,43 +46,33 @@ const FeaturedGames = () => {
       background_image,
     },
   ]
+  const mappedGames = TEST_ARR.map((game, index) => {
+    return (
+      <SwiperSlide key={index}>
+        <SwiperCard gameProps={game} />
+      </SwiperSlide>
+    )
+  })
   return (
-    <div className="flex items-center justify-center flex-col border border-white py-8">
-      <h1 className="text-center text-4xl mb-6">Featured Games</h1>
+    <div className="mb-8 flex flex-col items-center">
+      <h1 className="mb-6 text-center text-4xl text-white">Featured Games</h1>
       <Swiper
         breakpoints={{
           340: { slidesPerView: 2, spaceBetween: 13 },
-          700: { slidesPerView: 3, spaceBetween: 0 },
+          700: { slidesPerView: 3, spaceBetween: 15 },
         }}
         freeMode={true}
         pagination={{ clickable: true }}
         modules={[FreeMode, Pagination]}
-        className="max-w-[90%] lg:max-w-[80%]"
+        className="mb-5 max-w-[90%] lg:max-w-[70%]"
       >
-        {TEST_ARR.map((game, index) => {
-          return (
-            <SwiperSlide key={index}>
-              <div className="flex flex-col mb-20 gap-6 relative bg-neutral text-white rounded-lg h-[250px] w-[215px] lg:h-[350px] lg:w-[355px] transform transition-all scale-95 hover:scale-100 hover:cursor-pointer">
-                <div className="">
-                  <figure>
-                    <img
-                      src={game.background_image}
-                      alt="game image"
-                      className="rounded-lg"
-                    />
-                  </figure>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary btn-sm mt-4 mr-4">
-                      Learn More
-                    </button>
-                  </div>
-                  <h2 className="card-title pl-6">{game.name}</h2>
-                </div>
-              </div>
-            </SwiperSlide>
-          )
-        })}
+        {mappedGames}
       </Swiper>
+      <div>
+        <button className="btn btn-primary btn-wide text-white">
+          Explore more
+        </button>
+      </div>
     </div>
   )
 }
