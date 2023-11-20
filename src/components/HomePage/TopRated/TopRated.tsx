65ee494ff86613120c2c4dfef95089cac77a2434
filus4 <filej1025@gmail.com>
2023-11-20 02:52:28 +0100

    added top rated section

---
+++ src/components/HomePage/TopRated/TopRated.tsx
@@ -0,0 +1,52 @@
+import React from "react"
+import { useQuery } from "@apollo/client"
+
+import { GET_TOP_RATED } from "@/utils/graphQueries"
+import GameCard from "../Bestsellers/GameCard"
+import { topRatedListings } from "@/types/listingType"
+
+const TopRated = () => {
+  const { loading, error, data: listings } = useQuery(GET_TOP_RATED)
+
+  console.log(listings)
+  let mappedGames
+
+  if (listings) {
+    mappedGames = listings.listingsByGames.map((game: topRatedListings) => (
+      <GameCard
+        key={game.gameId}
+        gameId={game.gameId}
+        gameName={game.gameName}
+        gameImage={game.gameImage}
+        tags={game.tags}
+        genres={game.genres}
+        bgColor="neutral"
+      />
+    ))
+  }
+
+  return (
+    <>
+      <h1 className="mb-6 text-center text-4xl text-white">Top Rated</h1>
+      {loading && (
+        <div className="flex h-96 items-center justify-center">
+          <span className="loading loading-spinner loading-lg"></span>
+        </div>
+      )}
+      {!loading && !error && (
+        <>
+          <div className="mx-auto grid h-fit max-w-screen-xl grid-flow-col grid-cols-1 grid-rows-6 gap-y-6 px-4 lg:grid-cols-2 lg:grid-rows-3">
+            <>{mappedGames}</>
+          </div>
+          <div className="flex items-center justify-center">
+            <button className="btn btn-primary btn-wide mb-3 mt-6 text-white">
+              Explore more
+            </button>
+          </div>
+        </>
+      )}
+    </>
+  )
+}
+
+export default TopRated
