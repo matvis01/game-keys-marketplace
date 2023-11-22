f8859aab408b6ef28206a6ab5569f36de5129fe9
filus4 <filej1025@gmail.com>
2023-11-22 15:13:29 +0100

    implemented categories

--- src/components/HomePage/Categories/Categories.tsx
+++ src/components/HomePage/Categories/Categories.tsx
@@ -1,33 +1,34 @@
 import React, { useState } from "react"
 import { useQuery } from "@apollo/client"
 
-import { GET_ALL_FILTERS } from "@/utils/graphQueries"
+import { GET_ALL_GENRES } from "@/utils/graphQueries"
 import CategoryButton from "./CategoryButton"
 
 const Categories = () => {
-  const { loading, error, data: listings } = useQuery(GET_ALL_FILTERS)
+  const { loading, error, data: listings } = useQuery(GET_ALL_GENRES)
   const [isShowAll, setIsShowAll] = useState<boolean>(false)
 
-  function removeDuplicates(arr: string[]): string[] {
-    let uniqueArray: string[] = []
-
-    for (let element of arr) {
-      if (!uniqueArray.includes(element)) {
-        uniqueArray.push(element)
-      }
-    }
-
-    return uniqueArray
-  }
-
+  let mappedCategories: JSX.Element[] = []
   if (listings) {
-    const filteredGenres: string[] = removeDuplicates(
-      listings?.allFilter?.genres,
+    mappedCategories = listings?.genres?.map(
+      (genre: { name: string; numberOfGames: number }, index: number) => {
+        const name = genre.name
+        return (
+          <CategoryButton
+            key={index}
+            imgSrc={`/icons/categoryIcons/${
+              name === "Massively Multiplayer" ? "mmo" : name.toLowerCase()
+            }.svg`}
+            imgAlt={`${genre.name} icon`}
+            text={`${name === "Massively Multiplayer" ? "MMO" : name}`}
+          />
+        )
+      },
     )
-    console.log(filteredGenres)
-    // const filteredTags: string[] = removeDuplicates(listings?.allFilter?.tags)
-    // console.log(filteredTags)
   }
+
+  console.log(listings)
+
   return (
     <>
       <div
@@ -37,60 +38,7 @@ const Categories = () => {
           isShowAll ? "overflow-y-visible" : "overflow-hidden"
         } px-16`}
       >
-        <CategoryButton
-          imgSrc="/icons/categoryIcons/adventure.svg"
-          imgAlt="icon by Icons8"
-          text="Action"
-          number={1}
-        />
-        <CategoryButton
-          imgSrc="/icons/categoryIcons/rpg.svg"
-          imgAlt="icon by Icons8"
-          text="RPG"
-          number={1}
-        />
-        <CategoryButton
-          imgSrc="/icons/categoryIcons/strategy.svg"
-          imgAlt="icon by Icons8"
-          text="Strategy"
-          number={1}
-        />
-        <CategoryButton
-          imgSrc="/icons/categoryIcons/simulation.svg"
-          imgAlt="icon by Icons8"
-          text="Simulation"
-          number={1}
-        />
-        <CategoryButton
-          imgSrc="/icons/categoryIcons/platformer.svg"
-          imgAlt="icon by Icons8"
-          text="Platform"
-          number={1}
-        />
-        <CategoryButton
-          imgSrc="/icons/categoryIcons/indie.svg"
-          imgAlt="icon by Icons8"
-          text="Indie"
-          number={1}
-        />
-        <CategoryButton
-          imgSrc="/icons/categoryIcons/shooter.svg"
-          imgAlt="icon by Icons8"
-          text="Shooter"
-          number={1}
-        />
-        <CategoryButton
-          imgSrc="/icons/categoryIcons/casual.svg"
-          imgAlt="icon by Icons8"
-          text="Casual"
-          number={1}
-        />
-        <CategoryButton
-          imgSrc="/icons/categoryIcons/sports.svg"
-          imgAlt="icon by Icons8"
-          text="Sports"
-          number={1}
-        />
+        {mappedCategories}
       </div>
       <div className="flex items-center justify-center">
         <button
