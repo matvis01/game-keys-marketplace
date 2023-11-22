ea79342ba96c19bd759b9acee023855611a72853
filus4 <filej1025@gmail.com>
2023-11-22 15:47:44 +0100

    style changes

--- src/components/HomePage/Categories/CategoryButton.tsx
+++ src/components/HomePage/Categories/CategoryButton.tsx
@@ -17,7 +17,7 @@ const CategoryButton = ({
   height = 64,
 }: CategoryButtonProps) => {
   return (
-    <div className="flex h-36 w-36 flex-col items-center justify-center gap-4 bg-primary text-white shadow-lg transition-all duration-300 hover:cursor-pointer hover:bg-opacity-[75%]">
+    <div className="flex h-36 w-36 flex-col items-center justify-center gap-4 rounded-lg border border-primary bg-neutral text-white shadow-lg transition-all duration-300 hover:cursor-pointer hover:bg-primary">
       <Image src={imgSrc} alt={imgAlt} width={width} height={height} />
       <p>{text}</p>
     </div>
