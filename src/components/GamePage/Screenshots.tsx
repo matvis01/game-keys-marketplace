cbe8520577ac197e7afc5338d62ab6bab36503d7
matvis01 <mati.siwy@op.pl>
2023-11-28 17:07:38 +0100

    lepsze zdjecia

--- src/components/GamePage/Screenshots.tsx
+++ src/components/GamePage/Screenshots.tsx
@@ -1,29 +1,28 @@
 import { Screenshot } from "../../types/gameType"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Pagination, Navigation } from "swiper/modules"
+import "swiper/css/pagination"
 
 function Screenshots({ screenshots }: { screenshots: Screenshot[] }) {
   return (
     <div className=" w-1/2 justify-center">
-      <div className="carousel ">
-        {screenshots.map((screenshot, index) => {
-          return (
-            <div
-              id={screenshot.id.toString()}
-              className="carousel-item  aspect-video w-full object-cover"
-            >
-              <img key={index} src={screenshot.image} className="w-full" />
-            </div>
-          )
-        })}
-      </div>
-      <div className="flex w-full justify-center gap-2 py-2 ">
-        {screenshots.map((screenshot, index) => {
-          return (
-            <a href={"#" + screenshot.id.toString()} className="btn btn-xs">
-              {index + 1}
-            </a>
-          )
-        })}
-      </div>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {screenshots.map((screenshot, index) => (
+          <SwiperSlide key={index}>
+            <img src={screenshot.image} alt={screenshot.image} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   )
 }
