d2f0176dc3be40ce7702662a448f428383242ba8
matvis01 <mati.siwy@op.pl>
2023-12-05 19:58:12 +0100

    Create NavBarListItem.tsx

---
+++ src/components/Navigation/NavBarListItem.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import Link from "next/link"
+
+interface Props {
+  dataTestId?: string
+  styles?: string
+  text: string
+  href: string
+}
+
+const NavBarListItem = ({ styles, text, href, dataTestId }: Props) => {
+  return (
+    <li data-testid={dataTestId}>
+      <Link href={href}>
+        <p
+          className={`border-b border-b-transparent  text-xl font-semibold hover:border-b hover:border-b-primary ${styles}`}
+        >
+          {text}
+        </p>
+      </Link>
+    </li>
+  )
+}
+
+export default NavBarListItem
