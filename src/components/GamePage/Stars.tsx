798074c87b6c41ac3a5cba4f80e0aa892f812382
filus4 <filej1025@gmail.com>
2023-12-06 16:26:46 +0100

    added 2 arguments for dynamic size

--- src/components/GamePage/Stars.tsx
+++ src/components/GamePage/Stars.tsx
@@ -1,9 +1,24 @@
 import React from "react"
 
-function Stars({ rating, className }: { rating: number; className?: string }) {
+function Stars({
+  rating,
+  className,
+  small,
+  medium,
+}: {
+  rating: number
+  className?: string
+  small?: boolean
+  medium?: boolean
+}) {
   rating = Math.round(rating * 2) / 2
   return (
-    <div className={"rating rating-half rating-md " + className}>
+    <div
+      className={
+        `rating rating-half ${small ?? "rating-sm"} ${medium ?? "rating-md"}` +
+        className
+      }
+    >
       <input
         type="radio"
         className="mask mask-half-1 mask-star-2 cursor-default bg-primary"
